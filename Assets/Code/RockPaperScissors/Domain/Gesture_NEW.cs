5c6e9db8fea4c7d53ca6e94306f5ede4f37aaed4
Carlos Martinez <krlsmartnz@gmail.com>
2024-02-01 22:55:02 +0100

    feat: define gestures as value objects

---
+++ Assets/Code/RockPaperScissors/Domain/Gesture_NEW.cs
@@ -0,0 +1,14 @@
+﻿namespace RockPaperScissors.Domain
+{
+    public record Gesture_NEW
+    {
+        readonly string code;
+
+        Gesture_NEW(string code)
+            => this.code = code;
+
+        public static Gesture_NEW Rock => new("Rock");
+        public static Gesture_NEW Paper => new("Paper");
+        public static Gesture_NEW Scissors => new("Scissors");
+    }
+}
