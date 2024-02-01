6e73363d8992219a04bb5498219bdd6db9064a83
Carlos Martinez <krlsmartnz@gmail.com>
2024-02-01 19:59:39 +0100

    feat: create mars

---
+++ Assets/Code/MarsRover/Domain/Mars.cs
@@ -0,0 +1,14 @@
+﻿namespace MarsRover.Domain
+{
+    public class Mars
+    {
+        public int MaxX { get; }
+        public int MaxY { get; }
+
+        public Mars(int maxX, int maxY)
+        {
+            MaxX = maxX;
+            MaxY = maxY;
+        }
+    }
+}
---
+++ Assets/Code/MarsRover/Tests/MarsTests.cs
@@ -0,0 +1,18 @@
+﻿using FluentAssertions;
+using MarsRover.Domain;
+using NUnit.Framework;
+
+namespace MarsRover.Test
+{
+    public class MarsTests
+    {
+        [Test]
+        public void CreateMars()
+        {
+            var sut = new Mars(5, 5);
+            
+            sut.MaxX.Should().Be(5);
+            sut.MaxY.Should().Be(5);
+        }
+    }
+}
