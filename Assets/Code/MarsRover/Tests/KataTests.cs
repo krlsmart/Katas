247931342348b4ccdba3fdd53d0e87c980b652a1
Carlos Martinez <krlsmartnz@gmail.com>
2024-02-01 21:46:57 +0100

    test: ensure expected output of mars rover kata is satisfied

---
+++ Assets/Code/MarsRover/Tests/KataTests.cs
@@ -0,0 +1,24 @@
+﻿using FluentAssertions;
+using MarsRover.Domain;
+using NUnit.Framework;
+using static MarsRover.Domain.Cardinal;
+
+namespace MarsRover.Test
+{
+    public class KataTests
+    {
+        [Test]
+        public void FinalTest()
+        {
+            var mars = new Mars(5, 5);
+            var rover1 = new Rover(new Position(1, 2, N), mars);
+            var rover2 = new Rover(new Position(3, 3, E), mars);
+
+            rover1.Execute("LMLMLMLMM");
+            rover2.Execute("MMRMMRMRRM");
+
+            rover1.Position.Should().Be(new Position(1, 3, N));
+            rover2.Position.Should().Be(new Position(5, 1, E));
+        }
+    }
+}
