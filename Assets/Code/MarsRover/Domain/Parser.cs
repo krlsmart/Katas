f13f0f4772c02f665bc0e5c1c2ea213a20ec2797
Carlos Martinez <krlsmartnz@gmail.com>
2024-02-01 21:33:24 +0100

    feat: split a string into a enumerable of strings

---
+++ Assets/Code/MarsRover/Domain/Parser.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace MarsRover.Domain
+{
+    public static class Parser
+    {
+        public static IEnumerable<string> Split(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+                throw new System.ArgumentException("Invalid input");
+
+            return input.Select(s => s.ToString());
+        }
+    }
+}
---
+++ Assets/Code/MarsRover/Tests/ParserTests.cs
@@ -0,0 +1,32 @@
+﻿using FluentAssertions;
+using MarsRover.Domain;
+using NUnit.Framework;
+using System;
+using System.Linq;
+
+namespace MarsRover.Test
+{
+    public class ParserTests
+    {
+        [Test]
+        public void SplitInput()
+        {
+            var sut = Parser.Split("abc").ToList();
+
+            sut.Count.Should().Be(3);
+            sut[0].Should().Be("a");
+            sut[1].Should().Be("b");
+            sut[2].Should().Be("c");
+        }
+
+        [Test]
+        public void Split_NullOrEmptyInput_ThrowsArgumentException()
+        {
+            Action act1 = () => Parser.Split(null);
+            Action act2 = () => Parser.Split(string.Empty);
+
+            Assert.Throws<ArgumentException>(act1.Invoke);
+            Assert.Throws<ArgumentException>(act2.Invoke);
+        }
+    }
+}
