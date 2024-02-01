28b1fd46ffb348b37d9d3f88b686290faa6e1b9d
Carlos Martinez <krlsmartnz@gmail.com>
2024-02-01 22:06:35 +0100

    feat: create order domain concept

---
+++ Assets/Code/MarsRover/Domain/Order.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace MarsRover.Domain
+{
+    public class Order : IEquatable<Order>
+    {
+        readonly string code;
+
+        Order(string code)
+        {
+            this.code = code;
+        }
+
+        public static Order TurnLeft = new("L");
+        public static Order TurnRight = new("R");
+        public static Order Move = new("M");
+
+        public static Order By(string code)
+        {
+            if(!IsValid(code))
+                throw new NotSupportedException($"The order with code {code} is not supported.");
+
+            return new Order(code);
+        }
+
+        static bool IsValid(string order)
+        {
+            if (string.IsNullOrEmpty(order))
+                return false;
+
+            return order.Equals("L")
+                || order.Equals("R")
+                || order.Equals("M");
+        }
+
+        public bool Equals(Order other)
+        {
+            return this.code.Equals(other.code);
+        }
+    }
+}
