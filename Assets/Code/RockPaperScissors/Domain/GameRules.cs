106300182d9d29eab61f1ccf8314f6c5632a5d7c
Carlos Martinez <krlsmartnz@gmail.com>
2024-02-01 22:56:01 +0100

    feat: define game rules

---
+++ Assets/Code/RockPaperScissors/Domain/GameRules.cs
@@ -0,0 +1,19 @@
+﻿namespace RockPaperScissors.Domain
+{
+    public static class GameRules
+    {
+        public static bool Beats(this Gesture_NEW gesture, Gesture_NEW other)
+        {
+            if(gesture.Equals(Gesture_NEW.Rock) && other.Equals(Gesture_NEW.Scissors))
+                return true;
+
+            if (gesture.Equals(Gesture_NEW.Paper) && other.Equals(Gesture_NEW.Rock))
+                return true;
+
+            if (gesture.Equals(Gesture_NEW.Scissors) && other.Equals(Gesture_NEW.Paper))
+                return true;
+
+            return false;
+        }
+    }
+}
