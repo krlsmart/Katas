cf6e3be4d2a1f75de1525e56588bb430c60339b8
Carlos Martinez <krlsmartnz@gmail.com>
2024-02-15 21:09:26 +0100

    refactor: rename class that contains functions

--- Assets/Code/FunctionalProgramming/Domain/FirstClassFactory.cs
+++ Assets/Code/FunctionalProgramming/Domain/FirstClassFactory.cs
@@ -2,12 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
-//https://www.codewars.com/kata/563f879ecbb8fcab31000041
 namespace FunctionalProgramming.Domain
 {
-    public class FirstClass
+    public class Functions
     {
-        public static Func<IEnumerable<int>, IEnumerable<int>> Factory(int multiplier)
+        //https://www.codewars.com/kata/563f879ecbb8fcab31000041
+        public static Func<IEnumerable<int>, IEnumerable<int>> MultiplyBy(int multiplier)
             => array => array.Select(x => x * multiplier);
     }
 }
--- Assets/Code/FunctionalProgramming/Test/FirstClassTests.cs
+++ Assets/Code/FunctionalProgramming/Test/FirstClassTests.cs
@@ -9,7 +9,7 @@ namespace FunctionalProgramming.Test
         [Test]
         public void MultiplyBy()
         {
-            var sut = FirstClass.Factory(5);
+            var sut = Functions.MultiplyBy(5);
 
             var result = sut(new int[] { 1, 2, 3 });
 
@@ -20,9 +20,9 @@ namespace FunctionalProgramming.Test
         [Test]
         public void MultiplyBy_InOneLine()
         {
-            FirstClass.Factory(5)(new int[] { 1, 2 }).Should().BeEquivalentTo(new int[] { 5, 10 });
-            FirstClass.Factory(10)(new int[] { 1, 2 }).Should().BeEquivalentTo(new int[] { 10, 20 });
-            FirstClass.Factory(25)(new int[] { 1, 2 }).Should().BeEquivalentTo(new int[] { 25, 50 });
+            Functions.MultiplyBy(5)(new int[] { 1, 2 }).Should().BeEquivalentTo(new int[] { 5, 10 });
+            Functions.MultiplyBy(10)(new int[] { 1, 2 }).Should().BeEquivalentTo(new int[] { 10, 20 });
+            Functions.MultiplyBy(25)(new int[] { 1, 2 }).Should().BeEquivalentTo(new int[] { 25, 50 });
         }
     }
 }
