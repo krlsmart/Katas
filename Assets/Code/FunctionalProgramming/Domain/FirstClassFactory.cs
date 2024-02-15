89cba3126bcd25d3d3dedbfe1708e2fd763cfa6f
Carlos Martinez <krlsmartnz@gmail.com>
2024-02-15 21:10:18 +0100

    docs: comment next functions to continue with

--- Assets/Code/FunctionalProgramming/Domain/FirstClassFactory.cs
+++ Assets/Code/FunctionalProgramming/Domain/FirstClassFactory.cs
@@ -9,5 +9,14 @@ namespace FunctionalProgramming.Domain
         //https://www.codewars.com/kata/563f879ecbb8fcab31000041
         public static Func<IEnumerable<int>, IEnumerable<int>> MultiplyBy(int multiplier)
             => array => array.Select(x => x * multiplier);
+
+        //https://www.codewars.com/kata/the-takewhile-function
+        //public static Func<IEnumerable<int>> TakeWhile(IEnumerable<int> values, Func<bool> predicate)
+
+        //https://www.codewars.com/kata/the-dropwhile-function
+        //public static Func<IEnumerable<int>> DropWhile(IEnumerable<int> values, Func<bool> predicate)
+
+        //https://www.codewars.com/kata/the-span-function
+        //public static Func<IEnumerable<int>> Span(IEnumerable<int> values, Func<bool> predicate)
     }
 }
