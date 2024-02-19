f88488d084703aae92d94ff72852a2bee76a20d6
Carlos Martinez <krlsmartnz@gmail.com>
2024-02-19 20:36:17 +0100

    refactor: extract condition to iterate elements

--- Assets/Code/FunctionalProgramming/Domain/CustomLinq.cs
+++ Assets/Code/FunctionalProgramming/Domain/CustomLinq.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace FunctionalProgramming.Domain
@@ -7,19 +8,18 @@ namespace FunctionalProgramming.Domain
     {
         public static IEnumerable<int> Take(IEnumerable<int> values, int amount)
         {
-            return IterateElements(values, 0, amount);
+            return IterateElements(values, 0, (index) => index < amount);
         }
 
-        static IEnumerable<int> IterateElements(IEnumerable<int> values, int index, int amount)
+        static IEnumerable<int> IterateElements(IEnumerable<int> values, int index, Func<int, bool> condition)
         {
             if(index < values.Count())
             {
-                var result = IterateElements(values, index + 1, amount);
+                var result = IterateElements(values, index + 1, condition);
 
-                if(index < amount)
-                    return result.Prepend(values.ElementAt(index));
-
-                return result;
+                return condition(index)
+                    ? result.Prepend(values.ElementAt(index))
+                    : result;
             }
 
             return Enumerable.Empty<int>();
