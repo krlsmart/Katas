a949bf848b5d55c0dea52ab8bac4463fe6c64e9c
Carlos Martinez <krlsmartnz@gmail.com>
2024-02-19 20:28:36 +0100

    feat: take a specified amount of elements

---
+++ Assets/Code/FunctionalProgramming/Domain/CustomLinq.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace FunctionalProgramming.Domain
+{
+    public class CustomLinq
+    {
+        public static IEnumerable<int> Take(IEnumerable<int> values, int amount)
+        {
+            return IterateElements(values, 0, amount);
+        }
+
+        static IEnumerable<int> IterateElements(IEnumerable<int> values, int index, int amount)
+        {
+            if(index < values.Count())
+            {
+                var result = IterateElements(values, index + 1, amount);
+
+                if(index < amount)
+                    return result.Prepend(values.ElementAt(index));
+
+                return result;
+            }
+
+            return Enumerable.Empty<int>();
+        }
+    }
+}
---
+++ Assets/Code/FunctionalProgramming/Test/CustomLinqTests.cs
@@ -0,0 +1,33 @@
+﻿using FluentAssertions;
+using FunctionalProgramming.Domain;
+using NUnit.Framework;
+using System.Linq;
+
+namespace FunctionalProgramming.Test
+{
+    public class CustomLinqTests
+    {
+        [Test]
+        public void TakeZero()
+        {
+            CustomLinq.Take(new int[] { 1, 2, 3, 4 }, 0)
+                .Should().Equal(Enumerable.Empty<int>());
+        }
+
+        [Test]
+        public void TakeAmount()
+        {
+            CustomLinq.Take(new int[] { 1, 2, 3, 4, 5 }, 3)
+                .Should().Equal(new int[] { 1, 2, 3 });
+        }
+
+        [Test]
+        public void TakeAll()
+        {
+            var sequence = new int[] { 1, 2, 3, 4, 5 };
+
+            CustomLinq.Take(sequence, sequence.Count())
+                .Should().Equal(sequence);
+        }
+    }
+}
