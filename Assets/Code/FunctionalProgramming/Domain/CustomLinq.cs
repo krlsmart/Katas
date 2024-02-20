6bb38b26c53adf9529ea2e12ead7367cc0802f68
Carlos Martinez <krlsmartnz@gmail.com>
2024-02-20 20:44:24 +0100

    refactor: implement iterator in a way we dont need to store de result locally

--- Assets/Code/FunctionalProgramming/Domain/CustomLinq.cs
+++ Assets/Code/FunctionalProgramming/Domain/CustomLinq.cs
@@ -7,32 +7,23 @@ namespace FunctionalProgramming.Domain.CustomLinq
     public static class CustomLinq
     {
         public static IEnumerable<int> CTake(this IEnumerable<int> values, int amount)
-            => values.IterateElements((index) => index < amount);
+            => values.IterateConditionOnIndex((index) => index < amount, Enumerable.Empty<int>());
 
         //https://www.codewars.com/kata/the-takewhile-function
         public static IEnumerable<int> CTakeWhile(this IEnumerable<int> values, Func<int, bool> predicate)
-            => values.IterateWhileCondition(predicate, Enumerable.Empty<int>());
+            => values.IterateConditionOnValue(predicate, Enumerable.Empty<int>());
+
 
         #region Aux Methods
-        static IEnumerable<int> IterateElements(this IEnumerable<int> values, Func<int, bool> condition, int index = 0)
-        {
-            if (index < values.Count())
-            {
-                var result = IterateElements(values, condition, index + 1);
-
-                return condition(index)
-                    ? new int[] { values.ElementAt(index) }.Concat(result)
-                    : result;
-            }
-
-            return Enumerable.Empty<int>();
-        }
-
-        static IEnumerable<int> IterateWhileCondition(this IEnumerable<int> values, Func<int, bool> predicate, IEnumerable<int> result, int index = 0) 
-            => index < values.Count() && predicate(values.ElementAt(index))
-                ? values.IterateWhileCondition(predicate, result.Append(values.ElementAt(index)), index + 1)
+        static IEnumerable<int> IterateConditionOnIndex(this IEnumerable<int> values, Func<int, bool> predicate, IEnumerable<int> result, int index = 0)
+            => index < values.Count() && predicate(index)
+                ? values.IterateConditionOnIndex(predicate, result.Append(values.ElementAt(index)), index + 1)
                 : result;
 
+        static IEnumerable<int> IterateConditionOnValue(this IEnumerable<int> values, Func<int, bool> predicate, IEnumerable<int> result, int index = 0) 
+            => index < values.Count() && predicate(values.ElementAt(index))
+                ? values.IterateConditionOnValue(predicate, result.Append(values.ElementAt(index)), index + 1)
+                : result;
         #endregion
     }
 }
