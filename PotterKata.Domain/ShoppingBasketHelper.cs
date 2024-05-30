10784ce67975300e68b996ba72696fd3c1ae1ba4
Carlos Martínez <krlsmartnz@gmail.com>
2024-05-30 21:13:09 +0200

    feat(potter): implement basket helper to obtain repeated and not repeated books

---
+++ PotterKata.Domain/ShoppingBasketHelper.cs
@@ -0,0 +1,22 @@
+﻿namespace PotterKata.Domain;
+
+public class ShoppingBasketHelper
+{
+    public static IEnumerable<Book> RepeatedBooksOf(IEnumerable<Book> books)
+    {
+        var distinctBooks = NotRepeatedBooksOf(books);
+        var booksCopy = new List<Book>(books);
+
+        foreach(var book in distinctBooks) 
+            booksCopy.Remove(book);
+        
+        return booksCopy;
+    }
+    
+    public static IEnumerable<Book> NotRepeatedBooksOf(IEnumerable<Book> books)
+    {
+        return books.GroupBy(b => b)
+            .Where(g => g.Count() == 1)
+            .Select(g => g.Key);
+    }
+}
---
+++ PotterKata.Tests/ShoppingBasketHelperShould.cs
@@ -0,0 +1,23 @@
+﻿using FluentAssertions;
+using static PotterKata.Domain.Book;
+using static PotterKata.Domain.ShoppingBasketHelper;
+
+namespace PotterKata.Tests;
+
+public class ShoppingBasketHelperShould
+{
+    [Test]
+    public void Obtain_RepeatedBooks()
+    {
+        RepeatedBooksOf(new[] { Book1, Book1, Book2, Book2, Book3, Book4 })
+            .Should().BeEquivalentTo(new[] { Book1, Book1, Book2, Book2 });
+    }
+    
+    [Test]
+    public void Obtain_NotRepeatedBooks()
+    {
+        NotRepeatedBooksOf(new[] { Book1, Book1, Book2, Book2, Book3, Book4 })
+            .Should().BeEquivalentTo(new[] { Book3, Book4 });
+    }
+    
+}
